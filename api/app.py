32a9fce152d4d1ca98bc39a4b71fe94f86ad347b
itenpn <iten.pn@gmail.com>
2019-11-20 18:02:55 -0500

    Documentation

--- api/app.py
+++ api/app.py
@@ -1,6 +1,15 @@
 #!api/bin/python
-from flask import Flask, jsonify, request, abort, make_response
 
+"""
+Authors:
+Nicholas Iten
+Noah Limes
+Clark Godwin
+Jerry Qiu
+Seamus Scanlon
+"""
+
+from flask import Flask, jsonify, request, abort, make_response
 import rospy
 import rospkg
 import yaml 
@@ -15,6 +24,7 @@ import os
 import threading
 from std_msgs.msg import String
 
+#Global variables that store objects representing the JSON response
 global controls_depth_msg
 global state_depth_msg
 global bboxes_msg
@@ -23,8 +33,10 @@ global imu_msg
 global object_msg
 global switches_msg
 
+#Starts the initial flask server
 app = Flask(__name__)
 
+#Needed to permit Angular to interact with the server
 @app.route('/', methods=['OPTIONS'])
 def opt_respond():
     response = jsonify('{"pass"}')
@@ -32,11 +44,15 @@ def opt_respond():
     response.headers.add('Access-Control-Allow-Headers', '*')
     return response
 
+#Handles POST requests sent to the server
 @app.route('/', methods=['POST'])
 def respond():
     try:
+        #Checks if the JSON is supposed to be here
         if not request.json or not 'request' in request.json:
             abort(400)
+        
+        #Goes through the request to find which data to send back
         output = []
         for json_input in request.json['request']:
             if json_input['data'] == 'Controls_Depth':
@@ -60,6 +76,7 @@ def respond():
             if json_input['data'] == 'Switches':
                 output.append({'Switches' : switches_msg})
 
+        #Creates the JSON request and headers
         response = jsonify({'data' : output})
         response.headers.add('Access-Control-Allow-Origin', '*')
         response.headers.add('Access-Control-Allow-Headers', '*')
@@ -67,15 +84,18 @@ def respond():
     except Exception as e:
         print(e)
 
+#Tells the requester if they messed up the JSON Request
 @app.errorhandler(400)
 def invalid(error):
     return make_response(jsonify({'error':'Invalid JSON Request'}), 400)
 
+#some stuff for the node setup
 rpack = rospkg.RosPack()
 config_path = os.getcwd() + "/api/infoNode_cfg.yaml"
 pubs = {}
 cfg = {}
 
+#callbacks for the node
 def controls_depth_callback(msg):
     controls_depth_msg = yaml.load(str(msg))
 
@@ -97,16 +117,20 @@ def object_callback(msg):
 def switches_callback(msg):
     switches_msg = yaml.load(str(msg))
 
+#Gets the config
 def loadConfig():
     global cfg
     with open(config_path, 'r') as stream:
         cfg = yaml.load(stream)
 
 def start_node():
+    #Gets the config
     loadConfig()
 
+    #starts the node on a new thread
     threading.Thread(target=lambda: rospy.init_node('infoNode', disable_signals=True)).start()
 
+    #Subscribes the node to all of the topics we want
     controls_depth_sub = rospy.Subscriber(cfg['controls_depth_topic'], ControlStatus, controls_depth_callback)
     state_depth_sub = rospy.Subscriber(cfg['state_depth_topic'], Depth, state_depth_callback)
     bboxes_sub = rospy.Subscriber(cfg['bboxes_topic'], BoundingBoxes, bboxes_callback)
@@ -115,7 +139,7 @@ def start_node():
     object_sub = rospy.Subscriber(cfg['object_topic'], Object, object_callback)
     switches_sub = rospy.Subscriber(cfg['switches_topic'], SwitchState, switches_callback)
 
-
+#Start hte node and the server
 if __name__ == '__main__':
     start_node()
     app.run(host='0.0.0.0', port=5000)
